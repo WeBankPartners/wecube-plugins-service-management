ecf2204f908527b46a57547ed9bac174db670e6c
homiexie <624128114@qq.com>
2019-11-06 15:12:36 +0800

    #30 modify base url

--- service-management-ui/src/api/server.js
+++ service-management-ui/src/api/server.js
@@ -1,22 +1,22 @@
 import req from "./base";
 
-export const queryServiceRequest = data => req.post(`/service-management/service-requests/query`, data);
-export const getAllServiceRequest = () => req.get(`/service-management/service-requests/retrieve`);
-export const createServiceRequest = data => req.post(`/service-management/service-requests`, data);
-export const updateServiceRequest = data => req.put(`/service-management/service-requests/${data.id}/update`, data);
-export const getAllAvailableServiceTemplate = () => req.get(`/service-management/service-request-templates/available`);
-export const taskProcess = data => req.put(`/service-management/tasks/${data.taskId}/process`, data);
-export const queryTask = data => req.post(`/service-management/tasks/query`, data);
-export const taskTakeover = data => req.put(`/service-management/tasks/${data.taskId}/takeover`, data);
-export const getCurrentUserRoles = () => req.get(`/service-management/core-resources/users/current-user/roles`);
+export const queryServiceRequest = data => req.post(`/api/service-management/v1/service-requests/query`, data);
+export const getAllServiceRequest = () => req.get(`/api/service-management/v1/service-requests/retrieve`);
+export const createServiceRequest = data => req.post(`/api/service-management/v1/service-requests`, data);
+export const updateServiceRequest = data => req.put(`/api/service-management/v1/service-requests/${data.id}/update`, data);
+export const getAllAvailableServiceTemplate = () => req.get(`/api/service-management/v1/service-request-templates/available`);
+export const taskProcess = data => req.put(`/api/service-management/v1/tasks/${data.taskId}/process`, data);
+export const queryTask = data => req.post(`/api/service-management/v1/tasks/query`, data);
+export const taskTakeover = data => req.put(`/api/service-management/v1/tasks/${data.taskId}/takeover`, data);
+export const getCurrentUserRoles = () => req.get(`/api/service-management/v1/core-resources/users/current-user/roles`);
 
-export const getAllProcessDefinitionKeys = () => req.get(`/service-management/core-resources/workflow/process-definition-keys`);
-export const getAllAvailableServiceCatalogues = () => req.get(`/service-management/service-catalogues/available`);
-export const getServicePipelineByCatalogueId = id => req.get(`/service-management/service-pipelines/service-catalogues/${id}`);
-export const createServiceRequestTemplate = data => req.post(`/service-management/service-request-templates`, data);
-export const createServiceCatalogue = data => req.post(`/service-management/service-catalogues`, data);
-export const createServicePipeline = data => req.post(`/service-management/service-pipelines`, data);
-export const getAllRoles = () => req.get(`/service-management/core-resources/roles`);
+export const getAllProcessDefinitionKeys = () => req.get(`/api/service-management/v1/core-resources/workflow/process-definition-keys`);
+export const getAllAvailableServiceCatalogues = () => req.get(`/api/service-management/v1/service-catalogues/available`);
+export const getServicePipelineByCatalogueId = id => req.get(`/api/service-management/v1/service-pipelines/service-catalogues/${id}`);
+export const createServiceRequestTemplate = data => req.post(`/api/service-management/v1/service-request-templates`, data);
+export const createServiceCatalogue = data => req.post(`/api/service-management/v1/service-catalogues`, data);
+export const createServicePipeline = data => req.post(`/api/service-management/v1/service-pipelines`, data);
+export const getAllRoles = () => req.get(`/api/service-management/v1/core-resources/roles`);
 
 
 
