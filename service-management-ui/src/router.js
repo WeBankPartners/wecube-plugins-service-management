c741e05b4c45d96a9b821926a4349bfe31441d75
homiexie <624128114@qq.com>
2019-11-12 15:07:40 +0800

    #33 upgrade iview

--- service-management-ui/src/router.js
+++ service-management-ui/src/router.js
@@ -1,35 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import TaskManagement from "./views/Task-management.vue"
-import ServiceCatalog from "./views/Service-catalog.vue"
  
 
-// Vue.use(Router)
+Vue.use(Router)
 
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/task-management',
-//       name: 'TaskManagement',
-//       component: TaskManagement
-//     },
-//     {
-//       path: '/service-catalog',
-//       name: 'ServiceCatalog',
-//       component: ServiceCatalog
-//     }
-//   ]
-// })
+export default new Router({
+  routes: [
+    {
+      path: '/task-management',
+      name: 'TaskManagement',
+      component: () => import("./views/Task-management.vue")
+    },
+    {
+      path: '/service-catalog',
+      name: 'ServiceCatalog',
+      component: () => import("./views/Service-catalog.vue")
+    }
+  ]
+})
 
-export default [
-  {
-    path: '/task-management',
-    name: 'TaskManagement',
-    component: TaskManagement
-  },
-  {
-    path: '/service-catalog',
-    name: 'ServiceCatalog',
-    component: ServiceCatalog
-  }
-]
