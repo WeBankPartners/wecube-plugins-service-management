9f1b0c83f5b16b5b5f2acb0743a0a36d14cd38ac
haixinhuang <huanghaixin@126.com>
2019-12-26 00:15:59 +0800

    #15 Return the json with the format defined by Wecube

--- build/start.sh
+++ build/start.sh
@@ -7,7 +7,7 @@ db_schema=$3
 mkdir -p /log
 java -jar /service-management/service-management.jar  --server.address=0.0.0.0 --server.port=21000 \
 --spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \
---spring.datasource.url="jdbc:mysql://${db_host}:${db_port}/${db_schema}?characterEncoding=utf8&serverTimezone=Asia\/Shanghai" \
+--spring.datasource.url="jdbc:mysql://${db_host}:${db_port}/${db_schema}?characterEncoding=utf8&serverTimezone=Asia/Shanghai" \
 --spring.datasource.username=$4 \
 --spring.datasource.password=$5 \
 --service.management.wecube-core-address=$6 >> /log/service-management.log
