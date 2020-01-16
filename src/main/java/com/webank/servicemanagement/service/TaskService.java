6ca4c796e099fdec8ea1cffdde0811f26a3cbebb
haixinhuang <huanghaixin@126.com>
2020-01-16 11:33:38 +0800

    #73 Use Camel-Case to define data model's entity name

--- src/main/java/com/webank/servicemanagement/service/TaskService.java
+++ src/main/java/com/webank/servicemanagement/service/TaskService.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Lists;
-import com.webank.servicemanagement.domain.ServiceRequestTemplate;
 import com.webank.servicemanagement.domain.Task;
 import com.webank.servicemanagement.dto.CreateTaskRequestDto;
 import com.webank.servicemanagement.dto.CreateTaskRequestInputDto;
