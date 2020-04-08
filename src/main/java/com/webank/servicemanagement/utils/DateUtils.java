80fbf69e73a68621823384e1742a9cfa03b032d5
huanghaixin@126.com <huanghaixin@126.com>
2020-04-08 15:35:52 +0800

    #121 - Remove the useJDBCCompliantTimezoneShift=true from jdbc connection string

--- src/main/java/com/webank/servicemanagement/utils/DateUtils.java
+++ src/main/java/com/webank/servicemanagement/utils/DateUtils.java
@@ -6,8 +6,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
-import javax.xml.crypto.Data;
-
 import com.google.common.base.Strings;
 
 public class DateUtils {
