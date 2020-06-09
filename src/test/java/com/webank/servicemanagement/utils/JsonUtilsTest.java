520ae01a70bce43bcb20afe78e66a42bd8077326
huanghaixin@126.com <huanghaixin@126.com>
2020-06-09 11:31:23 +0800

    #126 - Fixed #126, remove fastjson

--- src/test/java/com/webank/servicemanagement/utils/JsonUtilsTest.java
+++ src/test/java/com/webank/servicemanagement/utils/JsonUtilsTest.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 
-import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 
 public class JsonUtilsTest {
@@ -13,14 +12,14 @@ public class JsonUtilsTest {
     @Test
     public void toStringTest() throws Exception {
         List<String> list = Lists.newArrayList("deny", "approval", "return");
-        String str = JSON.toJSONString(list);
+        String str = JsonUtils.toJsonString(list);
 
         System.out.println("list= " + list);
         System.out.println("str=" + str);
 
         List<String> list2 = new ArrayList<String>();
 
-        list2 = JSON.parseObject(str, list.getClass());
+        list2 = JsonUtils.toObject(str, list.getClass());
         System.out.println("list2= " + list2);
 
         assertThat(list).isEqualTo(list2);
